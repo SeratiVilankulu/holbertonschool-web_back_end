c22323e76fce05ee47bf956f5bb8bb0a61fcf635
SeratiVilankulu <160496471+SeratiVilankulu@users.noreply.github.com>
2024-09-16 09:33:06 +0200

    Update 6-http_express.js

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -1,14 +1,20 @@
 const express = require('express');
 
+// Create an Express app
 const app = express();
+
+// Define the port number
 const port = 1245;
 
+// Handle the root URL ('/')
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
+// Start the server on port 1245
 app.listen(port, () => {
-  //   console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Server running on port ${port}`);
 });
 
+// Export the app
 module.exports = app;
